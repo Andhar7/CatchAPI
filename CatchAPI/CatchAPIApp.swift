5025d3bbb468ff43251ad77dab67484d6b768f5c
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-13 18:09:51 +0100

    Initial Commit

---
+++ CatchAPI/CatchAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchAPIApp.swift
+//  CatchAPI
+//
+//  Created by Andrej Kling on 13.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CatchAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
